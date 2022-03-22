ee2b702d521f344a18acc0024111840666cddaa8
roychshao <roych.shao@gmail.com>
2022-03-22 15:19:49 +0800

    express.static

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('dist'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
