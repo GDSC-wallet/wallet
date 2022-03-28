be2b47f058aeaf642b8d853515ae0b61b78b84cb
roychshao <roych.shao@gmail.com>
2022-03-28 16:13:01 +0800

    json data writing

--- db_caller.js
+++ db_caller.js
@@ -7,18 +7,21 @@ const port = 3000
 // 問資料庫user是否存在
 function authenticate (id) {
     db_dealer.user_exist(id)
-    .then(results => {
-        console.log("authentication says:")
-        console.log(results);
-        if(results.length == 0) {
+        .then(results => {
+            console.log("authentication says:")
+            console.log(results);
+            console.log(results.length);
+            if(results.length == 0) {
+                console.log("return false");
+                return false;
+            } else {
+                console.log("return true");
+                return true;
+            }
+        }).catch(err => {
+            console.log("ERROR: " + err.message);
             return false;
-        } else {
-            return true;
-        }
-    }).catch(err => {
-        console.log("ERROR: " + err.message);
-        return false;
-    });
+        });
 }
 
 exports.authenticate = authenticate;
--- index.js
+++ index.js
@@ -16,67 +16,69 @@ app.get('/user/data/get' ,(req, res) => {
     db_dealer.get_wallet('1')    // 之後parse req得到要求的user id,暫定1
         .then(results => {
             var user_status;
-            stt = db_caller.authenticate('1');
-            if (stt === true) {
+            var stt = db_caller.authenticate('1');  // 目前會因為非同步執行使stt為false,等之後接起來再寫promise
+            if(stt)
                 user_status = "success";
-            } else {
+            else
                 user_status = "error";
-            }
-            console.log(user_status);
-            /*
+
             // 將資料轉成需要的JSON格式
             var response = {
                 status: user_status,
-                message:"info for user_id1"
+                message:"info for user_id1",
                 data:{
-                    user_id: results[0][0].user_id,
-                    user_name: results[0][0].username,
-                    nick_name: results[0][0].nick_name,
+                    user_id: results[0].user_id,
+                    user_name: results[0].username,
+                    nick_name: results[0].nick_name,
                     selected_wallet_id:"haven't done",
                     wallets:[
+                        /*
                         "wallet-a":{
-                            wallet_id: results[0][0].wallet_id,
-                            wallet_name: results[0][0].wallet_name,
-                            selected: "True" // true or false, True for now testing
+                            wallet_id: results[i].wallet_id,
+                            wallet_name: results[i].wallet_name,
+                            selected: "True", // true or false, True for now testing
                             records:[
                                 {
                                     record_id:"wallet-a-record-1",
-                                    wallet_record_tag_id: results[0][0].wallet_record_tag_id,
-                                    record_ordinary: results[0][0].record_ordinary,
-                                    record_name: results[0][0].record_name,
-                                    record_description: results[0][0].description,
-                                    record_amount: results[0][0].amount,
-                                    record_type: results[0][0].type,
-                                    record_date: results[0][0].date,
-                                    record_created_time: results[0][0].record_created_time
-                                },
-                                {
-                                    record_id:"wallet-a-record-2",
-                                    wallet_record_tag_id: results[0][1].wallet_record_tag_id,
-                                    record_ordinary: results[0][1].record_ordinary,
-                                    record_name: results[0][1]...
-                                    record_description:
-                                    record_amount:
-                                    record_type:
-                                    record_date:
-                                    record_created_time:
+                                    wallet_record_tag_id: results[i].wallet_record_tag_id,
+                                    record_ordinary: results[i].record_ordinary,
+                                    record_name: results[i].record_name,
+                                    record_description: results[i].description,
+                                    record_amount: results[i].amount,
+                                    record_type: results[i].type,
+                                    record_date: results[i].date,
+                                    record_created_time: results[i].record_created_time
                                 }
                             ]
-                        },
-                        "wallet-b":{
-                            wallet_id:,
-                            wallet_name:,
-                            selected: // 若selected==false則records為空陣列
-                            records:[    
-                            ]
                         }
+                        */
                     ]
                 }
             }
-            */
-            console.log(results);
+            for(let i = 0; i < results[0].wallet_length; ++i) {
+                var record_arr = [];
+                for(let j = 0; j < wallet[i].record_length; ++j) {
+                    var record_obj = {
+                        {
+                            record_id:"",
+                            wallet_record_tag_id:,
+                            record_ordinary:,
+                            record_name:,
+                            record_description:,
+                            record_amount:,
+                            record_type:,
+                            record_data:,
+                            record_created_time:,
+                            record_updated_time:
+                        }
+                    }
+                    record_arr += record_obj;
+                }
+                response.data.wallets[i].records += record_arr;
+            }
+            console.log(response);
             db_dealer.close_sql_connection();
-            res.send(results);
+            res.send(response);
         }).catch(err => {
             db_dealer.close_sql_connection();
             res.send(err);
