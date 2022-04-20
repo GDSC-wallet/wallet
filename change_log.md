375ec96231e6b115297eb75fb3df1ad49084defe
william <william.hsieh@traiwan.com>
2022-04-21 03:35:14 +0800

    0421

---
+++ change_log.md
@@ -0,0 +1,14 @@
+
+```md
+日期：0421 
+改動者：william 
+改動內容：實作3個API
+備註：不穩定，尚未測試連接database
+
+## 以下是目前已實作ＡＰＩ
+
+ - /oauth/google/login
+ - /api/signup
+ - /api/profile
+
+### 詳細ＡＰＩ文件請參考 api.md
---
+++ controllers/tag.js
@@ -0,0 +1,14 @@
+import db_caller from "../db_interact/db_caller.js";
+
+export const get_tag = async (req, res) => {
+    res.status(200).json({ result:"success" });
+};
+export const insert_tag = async (req, res) => {
+    res.status(201).json({ result:"success" });
+};
+export const update_tag = async (req, res) => {
+    res.status(201).json({ result:"success" });
+};
+export const delete_tag = async (req, res) => {
+    res.status(201).json({ result:"success" });
+};
