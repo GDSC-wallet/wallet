a9c4e419a36370edd79ec5193dd3b3befa4ea2cf
william <william.hsieh@traiwan.com>
2022-04-20 21:49:26 +0800

    william

--- controllers/user.js
+++ controllers/user.js
@@ -2,14 +2,14 @@ const secret = 'test';
 
 export const signUp = async (req, res) => {
   try {
-    res.status(201).json({ result, token });
+    res.status(201).json({ result:"success" });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
 export const getUserProfile = async (req, res) => {
   try {
-    res.status(201).json({ result, token });
+    res.status(201).json({ result:"success" });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
   }
