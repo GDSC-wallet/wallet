e2132cd1c42996b80ef393f54a478d7d34c8e094
andyjjrt <andyjjrt@gmail.com>
2022-07-13 15:53:13 +0800

    fix: swagger doc 50%, delete session when login failed

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -10,10 +10,13 @@ const secret = process.env.SECRET;
 const auth = async (req, res, next) => {
 
   try {
-    res.user = req.session?.passport?.user
+    res.user = req.session.passport.user
+    const expTime = req.session.cookie._expires
+    if(expTime < new Date().toISOString()) throw new Error();
     next();
   } catch (error) {
     console.log('error :', error);
+    req.session.destroy();
     res.status(401).json({ success: false, message: "session parse error" })
   }
 };
