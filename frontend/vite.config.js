ab10b469e7b500fcf3e084cfabf839cd7b533277
andyjjrt <andyjjrt@gmail.com>
2022-07-14 11:46:57 +0800

    fix(frontend): Sass variables again
    thanks to https://github.com/antfu/unplugin-vue-components/blob/main/src/core/resolvers/vuetify.ts and vite preprocessor

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,27 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [
+        // Vuetify
+        VuetifyResolver(),
+      ],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: "\n@import '@/sass/variables.scss'\n",
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
