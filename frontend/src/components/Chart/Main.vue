044869ba08d3c20e1ef1cc8ed1fb21f40472b6b2
andyjjrt <andyjjrt@gmail.com>
2022-07-14 14:50:54 +0800

    feat(frontend): Add test chart and create statics page

---
+++ frontend/src/components/Chart/Main.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <canvas :id="chartId"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  props: {
+    type: String,
+    data: Object,
+    options: Object,
+    chartId: String
+  },
+  data() {
+    return {
+      mychart: null
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const config = {
+        type: this.type,
+        data: this.data,
+        options: this.options
+      };
+      this.mychart = new Chart(
+        document.getElementById(this.chartId),
+        config
+      )
+      console.log(this.mychart)
+    }
+  },
+  watch: {
+    Options() {
+      this.mychart.options = this.Options
+      this.mychart.update('none')
+    },
+    ChartData() {
+      this.mychart.data = this.ChartData
+      this.mychart.update('none')
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/Statics.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <Chart :data="test" :options="{}" type="doughnut" chartId="123" />
+  </div>
+</template>
+
+<script>
+import Chart from "@/components/Chart/Main.vue"
+export default {
+  name: "Statics",
+  components: {
+    Chart: Chart
+  },
+  computed: {
+    test() {
+      return {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+    }
+  }
+}
+</script>
