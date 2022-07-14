7a831a057c003a7338809151676c8a6416487d92
andyjjrt <andyjjrt@gmail.com>
2022-07-14 12:41:22 +0800

    fix(frontend): Calendar record display

--- frontend/src/components/Calendar/Main.vue
+++ frontend/src/components/Calendar/Main.vue
@@ -1,26 +1,37 @@
 <template>
   <div>
-    <vc-date-picker v-model="date" is-expanded is-required @update:to-page="toPage" />
+    <vc-date-picker 
+      v-model="date"
+      :attributes="statusBar"
+      is-expanded
+      is-required
+      @update:to-page="toPage"
+    />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: "Calendar",
   data() {
     return {};
   },
-  mounted() {},
+  mounted() { },
   methods: {
     toPage(e) {
       // First check if date is equal to score's date,
       // because render will trigger this function.
-      if(e.month != this.date.getMonth() + 1 || e.year != this.date.getFullYear()) {
+      if (e.month != this.date.getMonth() + 1 || e.year != this.date.getFullYear()) {
         this.date = new Date(new Date(this.date.setMonth(e.month - 1)).setFullYear(e.year))
-      } 
+      }
     }
   },
   computed: {
+    ...mapGetters({
+      getRecords: "wallet/getRecords"
+    }),
     date: {
       get() {
         return this.$store.getters["calendar/getDate"];
@@ -29,6 +40,18 @@ export default {
         this.$store.dispatch("calendar/setDate", value)
       }
     },
+    statusBar() {
+      let incomeDates = [];
+      let expenseDates = [];
+      this.getRecords.map(record => {
+        if (record.record_type === "income") {
+          incomeDates.push(new Date(record.record_date))
+        } else if (record.record_type === "expense") {
+          expenseDates.push(new Date(record.record_date))
+        }
+      })
+      return [{bar: true, dates: incomeDates},{bar: "red", dates: expenseDates}]
+    }
   },
 };
 </script>
