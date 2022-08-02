c4763a0d861298544add0973f14ad1cd2b2552eb
ShiveyVer <marklin9133131@gmail.com>
2022-08-02 20:40:05 +0800

    fix: now show piecharts even when no records

--- frontend/src/views/Statics.vue
+++ frontend/src/views/Statics.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <MonthSelector v-model="date" />
-    <div v-if="isEmpty">
-      <Chart :data="nullData" :options="{}" type="doughnut" chartId="0" />
-    </div>
-    <div v-else>
-      <v-btn @click="type = 'income'" :disabled="type == 'income'">收入</v-btn>
-      <v-btn @click="type = 'expense'" :disabled="type == 'expense'">支出</v-btn>
-      <v-btn @click="type = 'all'" :disabled="type == 'all'">全部</v-btn>
-      <Chart :data="test" :options="{}" type="doughnut" chartId="123" />
-      <v-col cols="12" sm="6">
-        <RecordCard :records="handleRecord(getRecords)" showDate />
-      </v-col>
-    </div>
+    <v-card flat>
+      <v-card-text>
+        <v-row align="center">
+          <v-btn-toggle dense v-model="btnSelected">
+            <v-btn :disabled="btnSelected == 0">全部</v-btn>
+            <v-btn :disabled="btnSelected == 1">支出</v-btn>
+            <v-btn :disabled="btnSelected == 2">收入</v-btn>
+          </v-btn-toggle>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <Chart :data="chartData" type="doughnut" chartId="123" />
+    <v-col cols="12" sm="6">
+      <RecordCard :records="handleRecord(getRecords)" showDate />
+    </v-col>
   </div>
 </template>
 
@@ -31,7 +34,7 @@ export default {
   data() {
     return {
       date: new Date(),
-      type: "income",
+      btnSelected: 0,
       nullData: {
         labels: ["本月無資料"],
         datasets: [{
@@ -64,13 +67,36 @@ export default {
       getRecords: "wallet/getRecords",
       getAllWalletTags: "wallet/getAllWalletTags"
     }),
-    test() {
+    type() {
+      let arr = ['all', 'expense', 'income'];
+      if (!this.btnSelected)
+        return arr[0]
+      return arr[this.btnSelected];
+    },
+    chartData() {
+      let vm = this;
+      if (this.isEmpty) {
+        return this.nullData;
+      }
+      if (this.type == 'all') {
+        return {
+          labels: ["收入", "支出"],
+          datasets: [this.getBalance]
+        }
+      }
       return {
-        labels: this.getAllWalletTags.filter(tag => (tag.tag_type == this.type || this.type == 'all')).map(item => item.tag_name),
+        labels: this.getAllWalletTags.filter(tag => {
+          return tag.tag_type == this.type
+        }).map(item => {
+          return item.tag_name
+        }).filter((val, id, arr) => {
+          return vm.getData[arr.indexOf(val)] != 0
+        }),
         datasets: [{
-          label: 'My First Dataset',
           data: this.getData,
-          backgroundColor: this.getAllWalletTags.filter(tag => (tag.tag_type == this.type || this.type == 'all')).map(item => item.tag_color),
+          backgroundColor: this.getAllWalletTags.filter(tag => {
+            return tag.tag_type == this.type
+          }).map(item => item.tag_color),
           hoverOffset: 4,
         }]
       }
@@ -113,6 +139,22 @@ export default {
       if (sum == 0)
         return true;
       return false;
+    },
+    getBalance() {
+      const dataFilter = (type) => {
+        let vm = this;
+        return this.getRecords.filter(function (rec) {
+          return (rec.record_date.slice(0, 7) == vm.getRange && rec.record_type == type)
+        }).reduce((tmp, object) => {
+          return tmp + object.record_amount;
+        }, 0)
+      }
+      let ret = Object();
+      ret.label = "balance";
+      ret.data = [dataFilter("income"), dataFilter("expense")];
+      ret.backgroundColor = ['#E6746A', '#5C7FB3'];
+      ret.hoverOffset = 4;
+      return ret;
     }
   }
 }
