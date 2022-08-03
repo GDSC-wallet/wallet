3650769866d3a5853958e478f6aed59c6e36032e
ShiveyVer <marklin9133131@gmail.com>
2022-08-03 14:37:58 +0800

    fix:get code cleaner

--- frontend/src/views/Statics.vue
+++ frontend/src/views/Statics.vue
@@ -35,15 +35,6 @@ export default {
     return {
       date: new Date(),
       btnSelected: 0,
-      nullData: {
-        labels: ["本月無資料"],
-        datasets: [{
-          label: "NO_DATA",
-          data: [1],
-          backgroundColor: ["#5C7FB3"],
-          hoverOffset: 4,
-        }]
-      }
     }
   },
   methods: {
@@ -51,10 +42,10 @@ export default {
       console.log(v)
     },
     handleRecord(arr) {
-      let tmp;
-      let vm = this;
-      tmp = arr.filter(rec => ((rec.record_type == vm.type || vm.type == 'all') &&
-        rec.record_date.slice(0, 7) == vm.getRange))
+      let th = this;
+      let tmp = arr.filter(rec => rec.record_date.slice(0, 7) == th.getRange);
+      if (th.type != 'all')
+        tmp = arr.filter(rec => rec.record_type == th.type);
       tmp.sort(function (a, b) {
         if (a.record_date > b.record_date) return 1;
         return -1;
@@ -74,9 +65,17 @@ export default {
       return arr[this.btnSelected];
     },
     chartData() {
-      let vm = this;
+      let th = this;
       if (this.isEmpty) {
-        return this.nullData;
+        let nullData = {
+          labels: ["本月無資料"],
+          datasets: {
+            data: [1],
+            backgroundColor: ["#5C7FB3"],
+            hoverOffset: 4,
+          },
+        }
+        return nullData;
       }
       if (this.type == 'all') {
         return {
@@ -85,34 +84,37 @@ export default {
         }
       }
       return {
-        labels: this.getAllWalletTags.filter(tag => {
-          return tag.tag_type == this.type
-        }).map(item => {
-          return item.tag_name
-        }).filter((val, id, arr) => {
-          return vm.getData[arr.indexOf(val)] != 0
-        }),
+        labels: this.getLabels,
         datasets: [{
           data: this.getData,
-          backgroundColor: this.getAllWalletTags.filter(tag => {
-            return tag.tag_type == this.type
-          }).map(item => item.tag_color),
-          hoverOffset: 4,
+          backgroundColor: this.getColor,
         }]
       }
     },
+    getLabels() {
+      let ret = this.getAllWalletTags.filter(tag => {
+        return tag.tag_type == this.type
+      }).map(item => {
+        return item.tag_name
+      }).filter((val, id, arr) => {
+        return vm.getData[arr.indexOf(val)] != 0
+      });
+      return ret;
+    },
+    getColor() {
+      let ret = his.getAllWalletTags.filter(tag => tag.tag_type == this.type)
+      return ret.map(item => item.tag_color)
+    },
     getData() {
-      let validRecord = [];
-      let vm = this
-      validRecord = this.getRecords.filter(function (rec) {
-        return (rec.record_date.slice(0, 7) == vm.getRange && (rec.record_type == vm.type || vm.type == 'all'))
+      let th = this
+      let validRecord = this.getRecords.filter(function (rec) {
+        return (rec.record_date.slice(0, 7) == th.getRange && rec.record_type == th.type)
       })
       let arr = [];
-      for (let i = 0; i < vm.getAllWalletTags.length; i++) {
-        if (vm.getAllWalletTags[i].tag_type != vm.type && vm.type != 'all')
-          continue;
+      let validTags = th.getAllWalletTags.filter((tag) => { return tag.tag_type == th.type });
+      for (let i = 0; i < validTags.length; i++) {
         let sameTag = validRecord.filter(function (rec) {
-          return rec.wallet_record_tag_id == vm.getAllWalletTags[i].tag_id;
+          return rec.wallet_record_tag_id == validTags[i].tag_id;
         });
         arr.push(sameTag.reduce((tmp, object) => {
           return tmp + object.record_amount;
@@ -142,18 +144,18 @@ export default {
     },
     getBalance() {
       const dataFilter = (type) => {
-        let vm = this;
+        let th = this;
         return this.getRecords.filter(function (rec) {
-          return (rec.record_date.slice(0, 7) == vm.getRange && rec.record_type == type)
+          return (rec.record_date.slice(0, 7) == th.getRange && rec.record_type == type)
         }).reduce((tmp, object) => {
           return tmp + object.record_amount;
         }, 0)
       }
-      let ret = Object();
-      ret.label = "balance";
-      ret.data = [dataFilter("income"), dataFilter("expense")];
-      ret.backgroundColor = ['#E6746A', '#5C7FB3'];
-      ret.hoverOffset = 4;
+      let ret = {
+        data: [dataFilter("income"), dataFilter("expense")],
+        backgroundColor: ['#E6746A', '#5C7FB3'],
+        hoverOffset: 4,
+      }
       return ret;
     }
   }
