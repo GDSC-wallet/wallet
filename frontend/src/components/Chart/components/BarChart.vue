d2ede27ac19fe42e9255ddc565d4bd2fe11f621d
ShiveyVer <marklin9133131@gmail.com>
2022-08-09 10:16:38 +0800

    feat: new bar chart coming in

---
+++ frontend/src/components/Chart/components/BarChart.vue
@@ -0,0 +1,52 @@
+<template>
+  <Chart :config="config" :chartId="chartId" />
+</template>
+
+
+<script>
+
+import Chart from "../Main.vue"
+export default {
+  props: {
+    data: Object,
+    options: Object,
+    chartId: String,
+    max: Number,
+  },
+  data() {
+    return {
+      config: {
+        type: 'bar',
+        data: null,
+        options: {
+          radius: 0,
+          scales: {
+            y: {
+              min: null,
+              max: null
+            }
+          },
+        },
+      },
+    }
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.config.data = this.data;
+      this.config.options.scales.y.max = this.max * -1;
+      this.config.options.scales.y.min = this.max;
+    }
+  },
+  watch: {
+    data() {
+      this.init();
+    },
+  },
+  components: {
+    Chart: Chart
+  }
+}
+</script>
---
+++ frontend/src/components/Chart/components/DonutChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <Chart :config="config" :chartId="chartId" />
+</template>
+
+
+<script>
+
+import Chart from "../Main.vue"
+export default {
+  props: {
+    data: Object,
+    options: Object,
+    chartId: String
+  },
+  data() {
+    return {
+      config: {
+        type: 'doughnut',
+        data: this.data,
+        options: this.options
+      },
+      ctx: document.getElementById(this.chartId)
+    }
+  },
+  components: {
+    Chart: Chart
+  },
+  watch: {
+    data() {
+      this.config.data = this.data;
+    }
+  }
+}
+</script>
