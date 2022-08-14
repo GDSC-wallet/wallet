d0205921c09786d88df16e4bff6a754237be6b7c
roychshao <roych.shao@gmail.com>
2022-08-14 22:44:09 +0800

    [20220814] einvoice done

--- backend/controllers/einvoice.js
+++ backend/controllers/einvoice.js
@@ -9,9 +9,7 @@ export const getEinvoice = async (req, res, next) => {
     const { startDate, endDate, cardNo, cardEncrypt } = req.query;
     const version = 0.5;
     const cardType = "3J0002";
-    const expTimeStamp = 1000;
-    const action = "carrierInvChk";
-    const timeStamp = Math.floor(Date.now()/1000)+100; // suggested to add timestamp from 10 to 180
+    const expTimeStamp = 2147483647;
     const onlyWinningInv = "N";
     const uuid = process.env.UUID;
     const appID = process.env.APP_ID;
@@ -23,7 +21,7 @@ export const getEinvoice = async (req, res, next) => {
         method: 'post',
         url: url,
         data: qs.stringify({
-            action: action,
+            action: "carrierInvChk",
             appID: appID,
             cardEncrypt: cardEncrypt,
             cardNo: cardNo,
@@ -32,19 +30,68 @@ export const getEinvoice = async (req, res, next) => {
             expTimeStamp: expTimeStamp,
             onlyWinningInv: onlyWinningInv,
             startDate: startDate,
-            timeStamp: timeStamp,
+            timeStamp: Math.floor(Date.now()/1000)+100, // suggested to add timestamp from 10 to 180
             uuid: uuid,
             version: version
         }),
         headers: {
             'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
         }
-    }).then(response => {
-        console.log(response.data);
-        // 將收到的response.data.details[i].invNum用以查詢發票明細
-        res.status(201).json(response.data);
+    }).then( async result => {
+        var response = {
+            success: true,
+            message: "取得載具發票明細資料成功"
+            data: {
+                headers: []
+            }
+        };
+        // 將收到的result.data.details[i].invNum用以查詢發票明細
+        var headers = result.data.details;
+
+        for(let i = 0; i < headers.length; ++i) {
+            // transform date format
+            var year = (Number(headers[i].invDate.year)+1911).toString();
+            var month = (Number(headers[i].invDate.month) < 10) ?
+                '0'+headers[i].invDate.month :
+                headers[i].invDate.month;
+            var date = (Number(headers[i].invDate.date) < 10) ?
+                '0'+headers[i].invDate.date :
+                headers[i].invDate.date;
+            var invDate = year+'/'+month+'/'+date;
+
+            await axios({
+                method: 'post',
+                url: url,
+                data: qs.stringify({
+                    action: "carrierInvDetail",
+                    appID: appID,
+                    cardEncrypt: cardEncrypt,
+                    cardNo: cardNo,
+                    cardType: cardType,
+                    expTimeStamp: expTimeStamp,
+                    invDate: invDate,
+                    invNum: headers[i].invNum,
+                    timeStamp: Math.floor(Date.now()/1000)+100, // suggested to add timestamp from 10 to 180
+                    uuid: uuid,
+                    version: version
+                }),
+                headers: {
+                    'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+                }
+            }).then(result => {
+                console.log(result.data);
+                response.data.headers.push(result.data);
+            }).catch(err => { throw err; })
+        }
+        console.log(response);
+        res.status(201).json(response);
     }).catch(err => {
-        console.error(err);
-        res.status(400).json(err);
+        var response = {
+            success: false,
+            message: "取得載具發票明細資料失敗error: " + err.message,
+            data: {}
+        }
+        console.error(response);
+        res.status(400).json(response);
     })
 }
